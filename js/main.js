c45ed88ec86f8c1c29272bc737fa6d667d197491
Wlavell2016 <william.lavell@gmail.com>
2018-09-19 20:47:21 +0700

    fixed loading

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 //    $('.preloader').fadeOut('slow');
 // });
 $(window).on('load', function(){
-  $('.preloader').fadeOut('slow');
-  $('.preloader2').fadeOut('slow');
-  $('.preloader3').fadeOut('slow');
+  $('.preloader').delay(250).fadeOut('slow');
+  $('.preloader2').delay(250).fadeOut('slow');
+  $('.preloader3').delay(250).fadeOut('slow');
 });
 /*
 *    main.js
